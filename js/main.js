25ad445a379cc09d90246d8abd5be299a1675dfb
SarahTierney <tierney.s@live.com>
2016-03-17 15:22:32 -0400

    wrote code for .append

--- js/main.js
+++ js/main.js
@@ -4,4 +4,12 @@ grains.forEach(function (grain) {
   var $name = $('<h2>');
   var $img = $('<img>');
   var $desc = $('<p>');
+  var $li = $('<li>');
+
+  $img.attr('src', 'images/' +grain.img);
+  $desc.html(grain.desc);
+  $name.html(grain.name);
+
+  $li.append($img);
+  $li.append($name);
 })
